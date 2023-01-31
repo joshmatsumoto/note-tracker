c3bc67f482a8eeec2e05ac7905bc6aad9832681a
JCSH <joshmatsbux@gmail.com>
2023-01-30 16:43:58 -1000

    linked according documents

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -5,7 +5,7 @@ let newNoteBtn;
 let noteList;
 
 
-if (window.location.pathname === '.\notes.html') {
+if (window.location.pathname === 'notes.html') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
